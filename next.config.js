9c9518c82fe2d9bd2da96d8e55fabf6ebb50c9be
Maroof-Akram535 <maroofrana10@gmail.com>
2022-03-16 19:27:45 +0500

    Add CDN

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-const isProd  = 'production'
-module.exports = {
+ module.exports = {
   reactStrictMode: true,
   target: 'serverless',
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/Maroof-Akram535/Maroof-Akram535.github.io/main/' : '',
 
 }
